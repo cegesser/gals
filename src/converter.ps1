ac37fb92f2c0863b2ea65b06d480351dd5bec526
Gustavo Rossi Müller <gustavo.rm90@gmail.com>
2023-03-10 10:32:03 -0300

    Ajuste

--- src/converter.ps1
+++
@@ -1,5 +0,0 @@
-# forfiles /S /M *.java /C "PowerShell.exe -File C:\Users\supero\Desktop\gals\gals\src\converter.ps1 @path"
-$yourfile = $args[0]
-echo $yourfile
-$content = get-content -path $yourfile 
-$content | out-file $yourfile -encoding utf8
